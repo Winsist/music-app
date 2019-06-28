1d8ebc9a2e45c4e8cd616103aa3cda4954b923d0
wangmy <>
2019-06-28 18:13:59 +0800

    wmy

---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="m-header">
+      <header class="header">
+          <img src="../../common/images/main.png" alt="">
+          <span>Mengyun Music</span>
+      </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+@import "~common/stylus/variable"
+.m-header{
+    .header{
+        height .5rem;
+        line-height .5rem;
+        text-align center;
+        font-size .16rem;
+        img{
+            width .5rem;
+            vertical-align middle;
+        }
+        span{
+            display:inline-block;
+            color:$color-theme;
+        }
+    }
+}
+</style>
